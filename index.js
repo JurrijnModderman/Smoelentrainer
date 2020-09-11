430a7e8ed744175c21b56762d401114143d69800
Unknown <99053654@mydavinci.nl>
2020-09-11 09:25:57 +0200

    fixed so added the code again

---
+++ index.js
@@ -0,0 +1,45 @@
+function()
+var selectiefoto = [{
+    name: "Manuel Neuer",
+    picture: "Smoelentrainer/Images/Neuer.jpg"
+  },
+  {
+    name: "Lucas Hernandez",
+    picture: "Smoelentrainer/Images/Hernandez.jpg"
+  },
+  {
+    name: "Niklas Sule",
+    picture: "Smoelentrainer/Images/Sule.jpg"
+  },
+  {
+    name: "Robert Lewandowski",
+    picture: "Smoelentrainer/Images/Lewandowski.jpg"
+  },
+  {
+    name: "Benjamin Pavard",
+    picture: "Smoelentrainer/Images/Pavard.jpg"
+  },
+  {
+    name: "Ivan Perisic",
+    picture: "Smoelentrainer/Images/Perisic.jpg"
+  },
+  {
+    name: "Kingsley Coman",
+    picture: "Smoelentrainer/Images/Coman.jpg"
+  },
+  {
+    name: "Philippe Coutinho",
+    picture: "Smoelentrainer/Images/Coutinho.jpg"
+  },
+  {
+    name: "Correntin Tolisso",
+    picture: "Smoelentrainer/Images/Tolisso.jgp"
+  },
+  {
+    name: "Thiago Alcantara",
+    picture: "Smoelentrainer/Images/Thiago.jpg"
+  },
+  {
+    name: "Joshua Kimmich",
+    picture: "Smoelentrainer/Images/Kimmich.jpg"
+  },
